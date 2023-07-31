70508c2d0bba925e0bcb9e74dd759d268426e5f6
kimyerim01118 <72791856+yr0118kim@users.noreply.github.com>
2023-07-31 23:57:05 +0900

    0731

--- Note/src/app/page.tsx
+++ Note/src/app/page.tsx
@@ -1,8 +1,14 @@
+import Counter from "@/components/Counter";
 import Image from "next/image";
 import os from "os"; //노드 APIs
 
 export default function Home() {
   console.log("안녕!");
   console.log(os.hostname());
-  return <h1>홈페이지다!!!!</h1>;
+  return (
+    <>
+      <h1>홈페이지다!</h1>
+      <Counter/>
+    </>
+  );
 }
