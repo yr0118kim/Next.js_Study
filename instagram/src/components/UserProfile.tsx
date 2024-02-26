4ac3e0ed0391faf0950757b9ec3a6dfae52250d4
kimyerim01118 <72791856+yr0118kim@users.noreply.github.com>
2024-02-26 23:50:30 +0900

    0226

--- instagram/src/components/UserProfile.tsx
+++ instagram/src/components/UserProfile.tsx
@@ -1,8 +1,32 @@
 import { ProfileUser } from "@/model/user";
+import Avatar from "./Avatar";
+import FollowButton from "./FollowButton";
 
 type Props = {
   user: ProfileUser;
 };
 export default function UserProfile({ user }: Props) {
-  return <p>{user.username}</p>;
+  const { image, username, name, followers, following, posts } = user;
+  const info = [
+    { name: 'posts', data : posts },
+    { name: 'followers', data: followers },
+    { title: 'following', data: following },
+    
+  ]
+  return (
+    <section>
+      <Avatar image={image} highlight />
+      <div>
+        <h1>{username}</h1>
+        <FollowButton />
+        <ul>
+          {info.map((item, index) => <li key={index}>
+            <span>{ }</span>
+          </li>)}
+
+        </ul>
+
+      </div>
+    </section>
+  );
 }
